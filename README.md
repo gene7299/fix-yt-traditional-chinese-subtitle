1abb21fb37d4bc351687d7bfc2d9a1432f8aa25f
Frank <40611willy@gmail.com>
2023-11-20 16:07:08 +0800

    init: Initial for package fix-yt-traditional-chinese-subtitle

--- README.md
+++ README.md
@@ -1,70 +1,7 @@
-# Chrome Extension TypeScript Starter
+# fix-yt-traditional-chinese-subtitle
 
-![build](https://github.com/chibat/chrome-extension-typescript-starter/workflows/build/badge.svg)
+Corrected the issue that prevented the proper display of YouTube's automatic translation in Traditional Chinese.
 
-Chrome Extension, TypeScript and Visual Studio Code
+### Package for translate
 
-## Prerequisites
-
-* [node + npm](https://nodejs.org/) (Current Version)
-
-## Option
-
-* [Visual Studio Code](https://code.visualstudio.com/)
-
-## Includes the following
-
-* TypeScript
-* Webpack
-* React
-* Jest
-* Example Code
-    * Chrome Storage
-    * Options Version 2
-    * content script
-    * count up badge number
-    * background
-
-## Project Structure
-
-* src/typescript: TypeScript source files
-* src/assets: static files
-* dist: Chrome Extension directory
-* dist/js: Generated JavaScript files
-
-## Setup
-
-```
-npm install
-```
-
-## Import as Visual Studio Code project
-
-...
-
-## Build
-
-```
-npm run build
-```
-
-## Build in watch mode
-
-### terminal
-
-```
-npm run watch
-```
-
-### Visual Studio Code
-
-Run watch mode.
-
-type `Ctrl + Shift + B`
-
-## Load extension to chrome
-
-Load `dist` directory
-
-## Test
-`npx jest` or `npm run test`
+- [opencc-js](https://github.com/nk2028/opencc-js)
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,63 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
+import * as OpenCC from "opencc-js";
+
+setInterval(() => {
+  const menu = document.querySelector(".ytp-panel-menu")!;
+  const items = menu.querySelectorAll(".ytp-menuitem")!;
+  items.forEach((item) => {
+    const span = item.querySelector(".ytp-menuitem-label")!;
+
+    if (span.textContent === "中文（繁體）") {
+      (item as HTMLElement).style.display = "none";
+    }
+    if (span.textContent === "中文（簡體）") {
+      span.textContent = "[修復] 中文（繁體）";
+    }
+  });
+});
+
+const converter = OpenCC.Converter({ from: "cn", to: "twp" });
+let lastSubtitle = new Array<string>();
+
+setInterval(() => {
+  const segments = document.querySelectorAll(".ytp-caption-segment");
+
+  if (!segments) return;
+
+  segments.forEach((segment, index) => {
+    if (!segment.textContent) return;
+
+    if (segment.textContent === lastSubtitle[index]) return;
+
+    if (segment.textContent === "英文 (自動產生) >> 中文（簡體）") {
+      segment.textContent = "英文 (自動產生) >> [修復] 中文（繁體）";
+      return;
+    }
+
+    const converted = converter(segment.textContent);
+    segment.textContent = converted;
+    lastSubtitle[index] = converted;
+  });
+});
+
+setInterval(() => {
+  const contents = document.querySelectorAll(".ytp-menuitem-content");
+  if (!contents) return;
+
+  contents.forEach((content) => {
+    if (content.textContent === "英文 (自動產生) >> 中文（簡體）") {
+      content.textContent = "英文 (自動產生) >> [修復] 中文（繁體）";
+    }
+  });
+});
+
+setInterval(() => {
+  const menu = document.querySelector(".ytp-panel-menu")!;
+  const items = menu.querySelectorAll(".ytp-menuitem")!;
+  items.forEach((item) => {
+    const span = item.querySelector(".ytp-menuitem-label")!;
+
+    if (span.textContent === "英文 (自動產生) >> 中文（簡體）") {
+      span.textContent = "英文 (自動產生) >> [修復] 中文（繁體）";
+    }
+  });
 });
