b345aaa4d657dd17e1be17e228c92214500a290c
Frank <40611willy@gmail.com>
2023-11-26 01:39:39 +0800

    chore: Update popup for more extension info

--- src/popup.tsx
+++ src/popup.tsx
@@ -3,18 +3,32 @@ import { createRoot } from "react-dom/client";
 
 const Popup = () => {
   return (
-    <div
-      style={{ minWidth: "300px", display: "flex", flexDirection: "column" }}
-    >
-      Thanks for using Youtube 繁體自動翻譯修正 :)
-      <a
-        style={{ marginTop: "12px" }}
-        target="_blank"
-        href="https://github.com/Frank0945/fix-yt-traditional-chinese-subtitle"
-      >
-        Github
-      </a>
-    </div>
+    <main>
+      <img src="128.png" width={64} height={64} alt="icon" />
+      <span className="title">
+        Thanks for using Youtube 繁體自動翻譯修正 :)
+      </span>
+      <div className="btns">
+        <a target="_blank" href="https://www.buymeacoffee.com/yuanchuang">
+          請開發者喝杯咖啡
+        </a>
+
+        <a
+          target="_blank"
+          href="https://github.com/Frank0945/fix-yt-traditional-chinese-subtitle"
+        >
+          Github
+        </a>
+      </div>
+      <div className="footer">
+        <span>
+          聯絡開發者：
+          <br />
+          yuanchuang940@gmail.com
+        </span>
+        <span className="version">v1.0.3</span>
+      </div>
+    </main>
   );
 };
 
